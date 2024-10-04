fc1780eaa014df5adc9235d67d2d97388cc60402
alimkarimi <alimisakarimi@gmail.com>
2024-10-04 14:59:37 -0700

    updating folder names for forked repo

--- dataloading_scripts/open_flood_obs.py
+++ dataloading_scripts/open_flood_obs.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-gt = pd.read_csv('/Users/alim/Documents/ccai_floods/gt/FloodArchive.txt', encoding= 'unicode_escape', on_bad_lines='skip', 
+gt = pd.read_csv('/Users/alim/Documents/ccai_floods/flood_prediction/data/gt/FloodArchive.txt', encoding= 'unicode_escape', on_bad_lines='skip', 
                 sep= '\t')
 
 gt_kenya = gt[gt['Country'] == "Kenya"]
