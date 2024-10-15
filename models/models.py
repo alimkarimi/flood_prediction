3336350c6b50159cb7d54ab962e94cd1751db9f1
alimkarimi <alimisakarimi@gmail.com>
2024-10-14 21:12:44 -0700

    adding support for multiple statistical models in statistical_models()

--- models/models.py
+++ models/models.py
@@ -1,4 +1,7 @@
 import numpy as np
+
+from xgboost import XGBClassifier
+
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
@@ -7,14 +10,16 @@ from sklearn.metrics import accuracy_score
 
 from sklearn.svm import SVC
 
-from dataloading_scripts.feature_builder import df_pos_neg
+from dataloading_scripts.feature_builder import getFeatures, df_pos_neg
 
 print(df_pos_neg)
 
-class SVM_model():
-    def __init__(self, data = df_pos_neg):
-        self.train_ratio = 0.5
-        self.test_ratio = 0.5
+class statistical_model():
+    def __init__(self, data = df_pos_neg, model_type = 'svm'):
+        
+        self.model_type = model_type
+        self.test_ratio = 0.3
+        self.train_ration = 1 - self.test_ratio
         X = data['feature_vector']
         y = data['label'] # these are either 'tp' or 'tn'. use LabelEncoder() to convert them to numerical format
 
@@ -29,34 +34,36 @@ class SVM_model():
         self.X_test = np.vstack(self.X_test.to_list())
 
 
-    def fit_svm(self):
+    def fit_model(self, verbose=True):
         """
         This function instatiates SVM model with default parameters and fits to training data
         """
-        self.svm = SVC()
+        if self.model_type == 'svm':
+            self.model = SVC() # instantiate SVM model
+        if self.model_type == 'xgb':
+            self.model = XGBClassifier()
 
-        # apply standard scalar pre-processing transform:
-        # print("before standard scalar: ", self.X_train)
-        # print("shape of values", self.X_train.shape)
-
-        self.scaler = StandardScaler()
-        transformed_X_train = self.scaler.fit_transform(self.X_train)
-        #print("after standard scaler", transformed_X_train)
-        print(self.y_train)
-
-        self.svm.fit(X = transformed_X_train, y = self.y_train)
-    
-    def test_svm(self):
+        self.scaler = StandardScaler() # instantiate scaler object to transform training features to have 0 mean and 1 variance.
+        transformed_X_train = self.scaler.fit_transform(self.X_train) # fit and transform training data using scaler object
+        if verbose:
+            print(f"Starting to train {self.model_type}")
+        self.model.fit(X = transformed_X_train, y = self.y_train) # find optimal SVM parameters using the given hyperparameters
+        if verbose:
+            print(f"Done training {self.model_type}")
+    def test_model(self, verbose=False):
         """
         This function will test the fitted SVM model and compute the accuracy of predictions (correct predictions / total predictions).
 
-        We need to transform X_test using the transformation used for X_train
+        We first transform X_test using the transformation used for X_train, the use the svm.predict() method to generate predictions on the test dataset. 
         """
+        print("starting testing")
 
-        self.X_test = self.scaler.transform(self.X_test)
-        self.y_pred = self.svm.predict(self.X_test)
-        print("Precitions on X_test:", self.y_pred)
-        print("Ground truth data", self.y_test)
+        self.X_test = self.scaler.transform(self.X_test) # transform test data using transform for training data
+        
+        self.y_pred = self.model.predict(self.X_test) # run predictions on test set.
+        if verbose:
+            print("Precitions on X_test:", self.y_pred)
+            print("Ground truth data", self.y_test)
 
         accuracy = accuracy_score(y_true = self.y_test, y_pred = self.y_pred)
         print(f"Accuracy for SVM trained on {self.X_train.shape[0]} data points is {accuracy}")
@@ -64,8 +71,11 @@ class SVM_model():
         true_pos_counter = 0
         false_pos_counter = 0
         false_negative_counter = 0
-        # compute precision (true_pos / true_pos + false_neg)
+
+        # compute precision/recall metrics (true_pos / true_pos + false_neg). 
+
         for (pred, gt) in zip(self.y_pred, self.y_test):
+            # iterate through predictions and ground truth to understand which predictions are true positives, false positives, and false negatives
             if gt == 1 and pred == 0:
                 false_negative_counter += 1
 
@@ -75,6 +85,8 @@ class SVM_model():
             if gt == 0 and pred == 1:
                 false_pos_counter +=1
         
+        # print out precision and recall metrics:
+
         if true_pos_counter + false_pos_counter == 0:
             print("Cannot compute precision because there are no TPs or FPs in dataset")
         else:
@@ -87,10 +99,9 @@ class SVM_model():
 
 
 if __name__ == "__main__":
-    svm = SVM_model(data = df_pos_neg)
-
-    svm.fit_svm()
-    svm.test_svm()
-
-
     
+
+    model = statistical_model(data = df_pos_neg, model_type='xgb')
+
+    model.fit_model()
+    model.test_model()
