1347267497e88ebae3d5fc3a9ec42f77aae85083
alimkarimi <alimisakarimi@gmail.com>
2024-10-23 18:22:09 -0700

    default pca run params

--- eda/pca.py
+++ eda/pca.py
@@ -19,16 +19,29 @@ labels = np.vstack(labels)
 scaler = StandardScaler()
 obs_matrix_true_pos_scaled = scaler.fit_transform(obs_matrix_all)
 
-pca = PCA(n_components=2)
+pca = PCA(n_components=5)
 pc_components = pca.fit_transform(obs_matrix_true_pos_scaled)
 
-path = "./analysis/"
+path = "./eda/"
 fn = "PCA_true_pos_floods_1996.jpg"
 
-plt.scatter(x = pc_components[0: 24,0], y = pc_components[0: 24, 1], label='pos')
-plt.scatter(x = pc_components[24:,0], y = pc_components[24:, 1], label='neg')
+plt.scatter(x = pc_components[10000:,0], y = pc_components[10000:, 1], label='neg')
+plt.scatter(x = pc_components[0:10000,0], y = pc_components[0: 10000, 1], label='pos')
+
 plt.title("PCA on ERA5 Features")
 plt.xlabel("PC1")
 plt.ylabel("PC2")
 plt.legend()
+plt.savefig(path + fn,)
+plt.clf()
+
+# build scree plot:
+PC_values = np.arange(pca.n_components_) + 1
+plt.plot(PC_values, pca.explained_variance_ratio_, 'o-', linewidth=2, color='blue')
+plt.title('Scree Plot')
+plt.xlabel('Principal Component')
+plt.ylabel('Variance Explained')
+fn = "PCA_scree_plot.jpg"
 plt.savefig(path + fn)
+
+plt.clf()
